a2d81fe9242836ca3d74da3fe6703d0bf7bf938c
Arnotts33 <arnaudlht@gmail.com>
2024-10-31 22:20:06 +0100

    Switch Hero css to module.css

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -1,22 +1,20 @@
+import styles from "./Hero.module.css";
+
 function Hero() {
 	return (
-		<section className="hero">
-			<div className="hero__title-flex">
-				<h1 className="hero__title" data-aos="fade-up">
-					Arnaud Lahaut
-				</h1>
-				<h1 className="hero__title outlined" data-aos="fade-up">
+		<section className={styles.hero}>
+			<div className={styles.hero__title}>
+				<h1 data-aos="fade-up">Arnaud Lahaut</h1>
+				<h1 className={styles.outlined} data-aos="fade-up">
 					Creative
 				</h1>
-				<h1 className="hero__title" data-aos="fade-up">
-					Frontend
-				</h1>
-				<h1 className="hero__title outlined" data-aos="fade-up">
+				<h1 data-aos="fade-up">Frontend</h1>
+				<h1 className={styles.outlined} data-aos="fade-up">
 					Developer
 				</h1>
 			</div>
 			<img
-				className="hero__image"
+				className={styles.hero__image}
 				src="../src/assets/images/beach-bg.jpg"
 				alt="beach background"
 				width="600px"
