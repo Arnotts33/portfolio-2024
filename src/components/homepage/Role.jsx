916c66303aae5dddda4cd39d536c13ff6b9af454
Arnotts33 <arnaudlht@gmail.com>
2024-11-01 23:21:58 +0100

    Done with Role section responsive

--- src/components/homepage/Role.jsx
+++ src/components/homepage/Role.jsx
@@ -9,13 +9,13 @@ function Role() {
 						Once a <span>chef</span>, I am now a
 					</p>
 					<div className={styles.role__title}>
-						<h1>Junior Frontend Developer</h1>
+						<h1>Frontend Developer</h1>
 						<p>based in Bordeaux, France.</p>
 					</div>
 					<p>
 						I love creating and crafting impactful digital
 						experiences that engage and resonate with people through
-						creative design and development
+						creative design and development.
 					</p>
 					<button className={styles.role__btn}>My Story</button>
 				</div>
@@ -23,6 +23,7 @@ function Role() {
 					<img
 						className={styles.role__image}
 						src="../src/assets/images/arnaud-cook.webp"
+						loading="lazy"
 						alt="arnaud presenting a dish"
 					/>
 				</div>
