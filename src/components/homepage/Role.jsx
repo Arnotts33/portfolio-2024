264accfee7943fc61da1fe03cca5c113b25fd496
Arnotts33 <arnaudlht@gmail.com>
2024-11-24 18:33:53 +0100

    Added background smooth apparition on Role section

--- src/components/homepage/Role.jsx
+++ src/components/homepage/Role.jsx
@@ -1,9 +1,43 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import styles from "./Role.module.css";
 import About from "./About";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(ScrollTrigger);
 
 function Role() {
 	const [isToggleAbout, setIsToggleAbout] = useState(false);
+	const section = useRef(null);
+	const role = useRef(null);
+
+	useGSAP(() => {
+		const tl = gsap.timeline();
+
+		ScrollTrigger.create({
+			trigger: role.current,
+			markers: true,
+			start: "top 80%",
+			end: "bottom 0%",
+
+			onEnter: () => {
+				tl.to(section.current, {
+					duration: 2.0,
+					backgroundColor: "var(--bg-dark)",
+					ease: "power4.inOut",
+				});
+			},
+
+			onLeaveBack: () => {
+				tl.to(section.current, {
+					duration: 2.0,
+					backgroundColor: "var(--bg-light)",
+					ease: "power4.inOut",
+				});
+			},
+		});
+	});
 
 	function handleToggleAbout() {
 		setIsToggleAbout((prevToggleAbout) => !prevToggleAbout);
@@ -11,9 +45,9 @@ function Role() {
 
 	if (!isToggleAbout)
 		return (
-			<section className={styles.role__section} id="about">
+			<section className={styles.role__section} ref={section} id="about">
 				<div className={styles.role__container}>
-					<p>
+					<p ref={role}>
 						Once a <span>chef</span>, I am now a
 					</p>
 					<div className={styles.role__container_title}>
