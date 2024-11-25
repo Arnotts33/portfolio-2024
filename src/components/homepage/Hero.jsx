7c6eb34aeab5c7738f8434d3050f5ec201136acc
Arnotts33 <arnaudlht@gmail.com>
2024-11-25 11:47:20 +0100

    Added gsap animation to socials and scroll icons in Hero section

--- src/components/homepage/Hero.jsx
+++ src/components/homepage/Hero.jsx
@@ -14,25 +14,46 @@ function Hero() {
 	const heroContainer = useRef(null);
 	const heroImg = useRef(null);
 	const titles = useRef([]);
+	const socials = useRef(null);
+	const scrollIndicator = useRef(null);
 
 	useGSAP(() => {
 		const tl = gsap.timeline();
 
 		tl.from(heroImg.current, {
 			opacity: 0,
-			scale: 1.5,
+			scale: 1.3,
 			duration: 3.2,
 			ease: "power4.inOut",
-		}).from(
-			titles.current,
-			{
-				y: 300,
-				opacity: 0,
-				duration: 2,
-				ease: "power3.inOut",
-			},
-			"-=2.5"
-		);
+		})
+			.from(
+				titles.current,
+				{
+					y: 300,
+					opacity: 0,
+					duration: 2,
+					ease: "power3.inOut",
+				},
+				"-=2.5"
+			)
+			.from(
+				socials.current,
+				{
+					opacity: 0,
+					duration: 3,
+					ease: "power3.inOut",
+				},
+				"-=2.5"
+			)
+			.from(
+				scrollIndicator.current,
+				{
+					opacity: 0,
+					duration: 2.3,
+					ease: "power3.inOut",
+				},
+				"-=2.8"
+			);
 	}, []);
 
 	return (
@@ -73,7 +94,7 @@ function Hero() {
 				</div>
 			</div>
 
-			<div className={styles.hero__socials}>
+			<div className={styles.hero__socials} ref={socials}>
 				<a
 					href="https://github.com/Arnotts33"
 					target="_blank"
@@ -96,7 +117,10 @@ function Hero() {
 					<FontAwesomeIcon icon={faSquareInstagram} size="xl" />
 				</a>
 			</div>
-			<div className={styles.hero__scroll_indicator}>
+			<div
+				className={styles.hero__scroll_indicator}
+				ref={scrollIndicator}
+			>
 				<div className={styles.mouse}></div>
 			</div>
 		</section>
